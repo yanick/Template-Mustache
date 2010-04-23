0b91735dad706bfe03e0c28f616db7478cf0359b
Pieter Vande Bruggen <pvande@gmail.com>
2010-04-22 22:06:38 -0700

    Added documentation for Template::Mustache::Template.

--- lib/Template/Mustache/Template.pm
+++ lib/Template/Mustache/Template.pm
@@ -1,3 +1,9 @@
+# {Template} is the object wrapper for a Mustache template.  This class
+# handles parsing, compiling, and caching the raw template, as well as the
+# basics of rendering it.
+#
+# Odds are, you don't have a good reason to use this class directly.  See
+# {Mustache.render Mustache->render} instead.
 package Template::Mustache::Template;
 
 use strict;
@@ -5,12 +11,23 @@ use strict;
 use Template::Mustache::Parser    qw/ parse /;
 use Template::Mustache::Generator qw/ build /;
 
+use namespace::clean;
+
+# Creates a new {Template} instance.
+# @param $source [String] The template represented by this object.
+# @return [Template]
 sub new
 {
     my ($class, $source) = @_;
     return bless { source => ($source || '') }, __PACKAGE__;
 }
 
+# Compiles the template down its basic string operations and renders the
+# output using the given +$context+.  Subsequent calls to {#render} will not
+# require recompilation.
+# @param $context [Context] A context stack, representing the data to be
+#   interpolated into the template.
+# @return [String]
 sub render
 {
     my ($self, $context) = @_;
