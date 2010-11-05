6dfa9b9bc7c6f8420f1ad40970a86bf2f8033d27
Pieter van de Bruggen <pvande@gmail.com>
2010-11-05 04:32:38 -0700

    Adding some documentation to the Context stack.

--- lib/Template/Mustache/Context.pm
+++ lib/Template/Mustache/Context.pm
@@ -1,9 +1,15 @@
+# This class encapsulates the context stack used in rendering Mustache
+# templates.
+#
+# @api private
 package Template::Mustache::Context;
 
 use List::Util qw/ first /;
 
 use namespace::clean;
 
+# Creates a new context stack, preloaded with the optional +$initial+ frame.
+# @param $initial [Hash|Class|Object] The initial stack frame.  Optional.
 sub new
 {
     my ($class, $initial) = @_;
@@ -13,12 +19,18 @@ sub new
     }, $class;
 }
 
+# The frame most recently used in a {#get} call.
+# @attr accessor
 sub frame
 {
     my ($self) = @_;
     return $self->{frame};
 }
 
+# Walks the context stack, top to bottom, looking for a frame containing data
+# for the given +$name+.
+# @param $name [String] The name to lookup.
+# @returns [String] The stringified result, or the empty string if undef.
 sub get
 {
     my ($self, $name) = @_;
@@ -40,12 +52,17 @@ sub get
     return '';
 }
 
+# Pushes the given key/value pairs onto the stack as a new frame.
+# @param %hash New context data.
 sub set
 {
     my ($self, %hash) = @_;
     unshift @{$self->{stack}}, { %hash };
 }
 
+# Pushes the given context object onto the hash.  Hash references will be
+# cloned first, for isolation.
+# @param $ctx [Hash|Class|Object] The new context object.
 sub push
 {
     my ($self, $ctx) = @_;
@@ -53,12 +70,19 @@ sub push
     unshift @{$self->{stack}}, $ctx;
 }
 
+# Removes the topmost object from the context stack.
 sub pop
 {
     my ($self) = @_;
     shift @{$self->{stack}};
 }
 
+# Calls the {Template::Mustache#partial} method on the topmost subclass of
+# Template::Mustache on the stack.  The rendered partial is then indented as
+# appropriate, and returned.
+# @param $name [String] The name of the partial to render.
+# @param $padding [String] The leading whitespace to prepend.
+# @return [String] The rendered partial.
 sub partial
 {
     my ($self, $name, $padding) = @_;
