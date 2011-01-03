b44bb598751e602fa7dba1d1ae5b39e244f81c85
Pieter van de Bruggen <pvande@gmail.com>
2011-01-02 19:17:37 -0800

    Adding a light spritz of documentation.

--- lib/Template/Mustache.pm
+++ lib/Template/Mustache.pm
@@ -1,7 +1,17 @@
+# Template::Mustache is an implementation of the fabulous Mustache templating
+# language for Perl 5.8 and later.
+#
+# Information about the design and syntax of Mustache can be found
+# [here](http://mustache.github.com).
 package Template::Mustache;
 use strict;
 use warnings;
 
+# Renders a template with the given data.
+#
+# @param [String] $tmpl The template to render.
+# @param [Hash,Class,Object] $data Data to interpolated into the template.
+# @return [String] The fully rendered template.
 sub render {
     my ($receiver, $tmpl, $data) = @_;
     return $tmpl
