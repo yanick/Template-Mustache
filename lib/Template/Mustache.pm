b8e92ffc4602dfc9bb06cbf9362b04e6ea1885dc
Pieter van de Bruggen <pvande@gmail.com>
2011-01-04 16:02:10 -0800

    Simplifying and documenting the parse method.

--- lib/Template/Mustache.pm
+++ lib/Template/Mustache.pm
@@ -19,41 +19,69 @@ use CGI ();
 sub build_pattern {
     my ($otag, $ctag) = @_;
     return qr/
-        ((?:.|\n)*?)                # Capture the pre-tag content
+        (.*?)                       # Capture the pre-tag content
         ([ \t]*)                    # Capture the pre-tag whitespace
         (?:\Q$otag\E \s*)           # Match the opening of the tag
         (?:
             (=)   \s* (.+?) \s* = | # Capture Set Delimiters
             ({)   \s* (.+?) \s* } | # Capture Triple Mustaches
-            (\W?) \s* ((?:.|\n)+?)  # Capture everything else
+            (\W?) \s* (.+?)         # Capture everything else
         )
         (?:\s* \Q$ctag\E)           # Match the closing of the tag
-    /xm;
+    /xsm;
 }
 
+# @overload parse($tmpl)
+#   Creates an AST from the given template.
+#   @param [String] $tmpl The template to parse.
+#   @return [Array] The AST represented by the given template.
+#   @api private
+# @overload parse($tmpl, $delims)
+#   Creates an AST from the given template, with non-standard delimiters.
+#   @param [String] $tmpl The template to parse.
+#   @param [Array<String>[2]] $delims The delimiter pair to begin parsing with.
+#   @return [Array] The AST represented by the given template.
+#   @api private
+# @overload parse($tmpl, $delims, $section, $start)
+#   Parses out a section tag from the given template.
+#   @param [String] $tmpl The template to parse.
+#   @param [Array<String>[2]] $delims The delimiter pair to begin parsing with.
+#   @param [String] $section The name of the section we're parsing.
+#   @param [Int] $start The index of the first character of the section.
+#   @return [(String, Int)] The raw text of the section, and the index of the
+#       character immediately following the close section tag.
+#   @api internal
 sub parse {
     my ($tmpl, $delims, $section, $start) = @_;
     my @buffer;
 
-    $delims ||= [qw'{{ }}'];
-    my $pattern = build_pattern(@$delims);
-
+    # Build the pattern, and instruct the regex engine to begin at `$start`.
+    my $pattern = build_pattern(@{$delims ||= [qw'{{ }}']});
     my $pos = pos($tmpl) = $start ||= 0;
 
+    # Begin parsing out tags
     while ($tmpl =~ m/\G$pattern/gc) {
         my ($content, $whitespace) = ($1, $2);
         my $type = $3 || $5 || $7;
         my $tag  = $4 || $6 || $8;
 
+        # Buffer any non-tag content we have.
+        push @buffer, $content if $content;
+
+        # Grab the index for the end of the content, and update our pointer.
         my $eoc = $pos + length($content) - 1;
         $pos = pos($tmpl);
 
+        # A tag is considered standalone if it is the only non-whitespace
+        # content on a line.
         my $is_standalone = (substr($tmpl, $eoc, 1) || "\n") eq "\n" &&
                             (substr($tmpl, $pos, 1) || "\n") eq "\n";
 
-        push @buffer, $content;
-
-        if ($is_standalone && ($type !~ /^[\{\&]?$/)) {
+        # Standalone tags should consume the newline that follows them, unless
+        # the tag is of an interpolation type.
+        # Otherwise, any whitespace we've captured should be added to the
+        # buffer, and the end of content index should be advanced.
+        if ($is_standalone && ($type ne '{' && $type ne '&' && $type ne '')) {
             $pos += 1;
         } elsif ($whitespace) {
             $eoc += length($whitespace);
@@ -62,25 +90,36 @@ sub parse {
         }
 
         if ($type eq '!') {
-            # Do nothing...
+            # Comment Tag - No-op.
         } elsif ($type eq '{' || $type eq '&' || $type eq '') {
-            push @buffer, [$type, $tag];
+            # Interpolation Tag - Buffers the tag type and name.
+            push @buffer, [ $type, $tag ];
+        } elsif ($type eq '>') {
+            # Partial Tag - Buffers the tag type, name, and any indentation
+            push @buffer, [ $type, $tag, $whitespace ];
+        } elsif ($type eq '=') {
+            # Set Delimiter Tag - Changes the delimiter pair and updates the
+            # tag pattern.
+            $pattern = build_pattern(@{$delims = [ split(/\s+/, $tag) ]});
         } elsif ($type eq '#' || $type eq '^') {
+            # Section Tag - Recursively calls #parse (starting from the current
+            # index), and receives the raw section string and a new index.
+            # Buffers the tag type, name, the section string and delimiters.
             (my $raw, $pos) = parse($tmpl, $delims, $tag, $pos);
             push @buffer, [ $type, $tag, [$raw, $delims] ];
         } elsif ($type eq '/') {
+            # End Section Tag - Short circuits a recursive call to #parse,
+            # returning the raw section template and the index immediately
+            # following this tag.
             return (substr($tmpl, $start, $eoc + 1 - $start), $pos);
-        } elsif ($type eq '>') {
-            push @buffer, [ $type, $tag, $whitespace ];
-        } elsif ($type eq '=') {
-            $pattern = build_pattern(@{$delims = [ split(/\s+/, $tag) ]});
         }
 
+        # Update our match pointer to coincide with any changes we've made.
         pos($tmpl) = $pos
     }
 
+    # Buffer any remaining template, and return a reference to the buffer.
     push @buffer, substr($tmpl, $pos);
-
     return \@buffer;
 }
 
