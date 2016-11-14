6044160fef8cecc99766c8e6eac682b74f77d5e4
Yanick Champoux <yanick@babyl.dyndns.org>
2016-11-13 23:28:17 -0500

    bump version

--- lib/Template/Mustache.pm
+++ lib/Template/Mustache.pm
@@ -30,7 +30,7 @@ use HTML::Entities;
 use File::Spec;
 use Scalar::Util 'blessed';
 
-use version 0.77; our $VERSION = qv('v0.5.4');
+use version 0.77; our $VERSION = qv('v0.5.5');
 
 my %TemplateCache;
 
