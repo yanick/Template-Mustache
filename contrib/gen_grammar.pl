f98312d52e6584fb91ad89a3568cb57bf8d7c606
Yanick Champoux <yanick@babyl.dyndns.org>
2017-09-27 17:58:46 -0400

    put a newline after package definition for #47

--- contrib/gen_grammar.pl
+++ contrib/gen_grammar.pl
@@ -1,6 +1,14 @@
 use Parse::RecDescent;
 use Template::Mustache;
+
+use Path::Tiny;
  
 Parse::RecDescent->Precompile( 
     $Template::Mustache::GRAMMAR, "Template::Mustache::Parser"
 );
+
+path('Parser.pm')->move( 'lib/Template/Mustache/Parser.pm' );
+
+my $package = path( './lib/Template/Mustache/Parser.pm' );
+
+$package->spew( map { s/^package.*?;/$&\n\n/r } $package->lines );
